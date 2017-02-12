0438fe249b17b0257989e7feea0775ba8af9a243
BDoctor <BenjaminDoctor@Yahoo.com>
2017-02-12 09:24:04 -0500

    Fixed references bugs

--- SpaceAdventure/Actor.cs
+++ SpaceAdventure/Actor.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using SpaceAdventure.Abstractions;
-
+using SpaceAdventure.Common;
 
 namespace SpaceAdventure
 {
--- SpaceAdventure/TileRoom.cs
+++ SpaceAdventure/TileRoom.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 
+using SpaceAdventure.Common;
+
 namespace SpaceAdventure
 {
     class TileRoom
